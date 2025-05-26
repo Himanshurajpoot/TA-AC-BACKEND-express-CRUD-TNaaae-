d68524738ebf4e35f0d994eadc8c988f13e47bca
Himanshu <harryrajpoot2003@gmail.com>
2025-05-26 17:08:42 +0530

    completed

---
+++ block-BNaada/sample/routes/user.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let router = express.Router();
+let User = require('../models/User');
+
+router.get('/new', (req, res) => {
+  res.render('userForm');
+});
+
+router.get('/', async (req, res) => {
+  try {
+    let users = await User.find({});
+    console.log(users);
+    res.render('users', { users: users });
+  } catch (err) {
+    console.error(err + 'something want wrong');
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.redirect('/users');
+    console.log(user);
+  } catch (err) {
+    res.redirect('/users/new');
+    console.error(err + 'not create the user');
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    let id = req.params.id;
+    let user = await User.findById(id);
+    res.render('singleUser', { user});
+  } catch (err) {
+    console.error(err + 'something wrong find single user');
+  }
+});
+
+module.exports = router;
